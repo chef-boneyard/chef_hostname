35af659a03e8955030d46ce7563afa7815c2e2f5
Tim Smith <tsmith@chef.io>
2016-12-02 15:46:29 -0800

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/unit/recipes/default.rb
+++ test/fixtures/cookbooks/unit/recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: fake
+# Cookbook:: fake
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright:: 2016, The Authors, All Rights Reserved.
 
 hostname "foobar.example.com"
 
