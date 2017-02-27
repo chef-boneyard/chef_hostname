24776c9ea403a82b5aecc6b1a4a829e892f4db95
Tim Smith <tsmith@chef.io>
2017-02-27 11:25:53 -0800

    Release 0.5.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@chef.io"
 license          "Apache 2.0"
 description      "Configures the hostname on a node"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.4.2"
+version          "0.5.0"
 source_url       "https://github.com/lamont-cookbooks/chef_hostname"
 issues_url       "https://github.com/lamont-cookbooks/chef_hostname/issues"
 
