b091969f4828fa1116da67bed2b99566e3941659
Tim Smith <tsmith@chef.io>
2017-05-30 14:55:38 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "chef_hostname"
 maintainer       "Chef Software, Inc."
 maintainer_email "cookbooks@chef.io"
-license          "Apache 2.0"
+license          "Apache-2.0"
 description      "Configures the hostname on a node"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.5.0"
@@ -11,6 +11,6 @@ issues_url       "https://github.com/lamont-cookbooks/chef_hostname/issues"
 chef_version     ">= 12.7" if respond_to?(:chef_version)
 
 %w{redhat centos scientific oracle fedora ubuntu debian linuxmint suse opensuse opensuseleap freebsd netbsd mac_os_x
-   solaris gentoo arch nexus}.each do |platform|
+   solaris2 gentoo arch nexus}.each do |platform|
   supports platform
 end
